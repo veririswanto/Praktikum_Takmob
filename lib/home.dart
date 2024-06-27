99c51ffd9faf7b6855220ff6e26d985bf2456792
veri <veririswanto3@gmail.com>
2024-06-27 19:40:51 +0700

    prak4

---
+++ lib/home.dart
@@ -0,0 +1,101 @@
+// Copyright 2018-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  // TODO: Make a collection of cards (102)
+  // TODO: Add a variable for Category (104)
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Return an AsymmetricView (104)
+    // TODO: Pass Category variable to AsymmetricView (104)
+    return  Scaffold(
+      // TODO: Add app bar (102)
+      appBar: AppBar(
+        title: const Text(
+          "ini Judul",
+          style:TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.white,
+            semanticLabel: "Menu Button",
+          ),
+          onPressed: () {
+            print("menu Clicked");
+          }
+        ),
+        // TODO: Add trailing buttons (102)
+actions: <Widget>[
+  IconButton(
+    icon: const Icon(
+      Icons.search,
+      semanticLabel: 'search',
+    ),
+    onPressed: () {
+      print('Search button');
+    },
+  ),
+  IconButton(
+    icon: const Icon(
+      Icons.tune,
+      semanticLabel: 'filter',
+    ),
+    onPressed: () {
+      print('Filter button');
+    },
+  ),
+],
+      ),
+      // TODO: Add a grid view (102)
+      body: GridView.count(
+  crossAxisCount: 2,
+  padding: const EdgeInsets.all(16.0),
+  childAspectRatio: 8.0 / 9.0,
+  // TODO: Build a grid of cards (102)
+  children: <Widget>[
+  Card(
+    clipBehavior: Clip.antiAlias,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        AspectRatio(
+          aspectRatio: 18.0 / 11.0,
+          child: Image.asset('assets/diamond.png'),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('Title'),
+              const SizedBox(height: 8.0),
+              Text('Secondary Text'),
+            ],
+          ),
+        ),
+      ],
+    ),
+  ),
+],
+      // TODO: Set resizeToAvoidBottomInset (101)
+    );
+  }
+}
